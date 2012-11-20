5238bcbf783ab105e796b8ac2c81778f3609c21d
Warren Bain <warren@thoughtcroft.com>
2012-11-21 05:50:12 +1100

    Fix error with method call for max time

--- lib/chef/handler/elapsed_time.rb
+++ lib/chef/handler/elapsed_time.rb
@@ -18,18 +18,17 @@ class Chef
       end
 
       def report
-        @max_time = all_resources.max_by{ |r| r.elapsed_time}.elapsed_time
-        @max_resource = all_resources.max_by{ |r| full_name(r).length}
-        Chef::Log.info "%-#{max_resource_length}s  %s"%["Resource", "Elapsed Time"]
-        Chef::Log.info "%-#{max_resource_length}s  %s"%["========", "============"]
-        all_resources.each do |r|
+        @max_time = all_resources.max_by{ |r| r.elapsed_time }.elapsed_time
+        @max_length = full_name(all_resources.max_by{ |r| full_name(r).length }).length
+        Chef::Log.info "%-#{@max_length}s %s"%["Resource", "Elapsed Time"]
+        Chef::Log.info "%-#{@max_length}s %s"%["========", "============"]
+        all_resources.sort_by{ |r| r.elapsed_time }.each do |r|
           char = if r.updated then "+" else "-" end
-          bar = char * ( @config[:max_width] * (r.elapsed_time/@max_time))
-          Chef::Log.info "%-#{max_resource_length}s  %s"%[full_name(r), bar]
+          bar = char * ( @config[:max_width] * (r.elapsed_time/@max_time)).ceil
+          Chef::Log.info "%-#{@max_length}s %.3fs %s"%[full_name(r), r.elapsed_time, bar]
         end
         Chef::Log.info ""
-        Chef::Log.info "Slowest Resource : #{full_name(@max_resource)} (%.6fs)"%[@max_time]
-        Chef::Log.info "Scale            : %.6fs per unit width"%[unit_width]
+        Chef::Log.info "Scale            : %.3fs per unit width"%[unit_width]
         Chef::Log.info " * '+' denotes a resource which updated this run"
         Chef::Log.info " * '-' denotes a resource which did not update this run"
       end
@@ -38,10 +37,6 @@ class Chef
 
     def unit_width
       @max_time/@config[:max_width]
-    end
-
-    def max_resource_length
-      full_name(@max_resource).length
     end
 
     def full_name(resource)
