83673429cdf6b82bb60c1190de41c804693a9a28
Warren Bain <warren@thoughtcroft.com>
2013-01-09 12:57:50 +1100

    Put graph at front for better readibility

--- lib/chef/handler/elapsed_time.rb
+++ lib/chef/handler/elapsed_time.rb
@@ -14,21 +14,21 @@ class Chef
       def initialize(config={})
         @config = config
         @config[:max_width] ||= 30
+        @config[:max_name] ||= 120 - @config[:max_width]
         @config
       end
 
       def report
         @max_time = all_resources.max_by{ |r| r.elapsed_time }.elapsed_time
-        @max_length = full_name(all_resources.max_by{ |r| full_name(r).length }).length
-        Chef::Log.info "%-#{@max_length}s %s"%["Resource", "Elapsed Time"]
-        Chef::Log.info "%-#{@max_length}s %s"%["========", "============"]
+        Chef::Log.info "%-#{@config[:max_width]}s %-#{@config[:max_name]}s"%["Elapsed Time", "Resource"]
+        Chef::Log.info "%-#{@config[:max_width]}s %-#{@config[:max_name]}s"%["============", "========"]
         all_resources.sort_by{ |r| r.elapsed_time }.each do |r|
           char = if r.updated then "+" else "-" end
           bar = char * ( @config[:max_width] * (r.elapsed_time/@max_time)).ceil
-          Chef::Log.info "%-#{@max_length}s %.3fs %s"%[full_name(r), r.elapsed_time, bar]
+          Chef::Log.info "%.3fs %s %-#{@config[:max_name]}s"%[r.elapsed_time, bar, full_name(r)]
         end
         Chef::Log.info ""
-        Chef::Log.info "Scale            : %.3fs per unit width"%[unit_width]
+        Chef::Log.info "Scale : %.3fs per unit width"%[unit_width]
         Chef::Log.info " * '+' denotes a resource which updated this run"
         Chef::Log.info " * '-' denotes a resource which did not update this run"
       end
