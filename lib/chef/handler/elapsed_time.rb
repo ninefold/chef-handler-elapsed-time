88573be1668e29869854d1b4a801b7c4644ae1fb
Warren Bain <warren@ninefold.com>
2014-03-20 22:51:19 +1100

    Namespace as per other chef handlers

--- lib/chef/handler/elapsed_time.rb
+++ lib/chef/handler/elapsed_time.rb
@@ -5,47 +5,61 @@
 # Copyright:: Copyright 2012 Opscode, Inc.
 # License:: Apache2
 #
+# Adapted:: Warren Bain @ Ninefold
+#
 
+require 'rubygems'
+Gem.clear_paths
+require 'chef'
+require 'chef/log'
+require 'chef/handler'
 
-class Chef
-  class Handler
-    class ElapsedTime < Chef::Handler
+module Ninefold
+  module Handler
+    class ElapsedTime < ::Chef::Handler
+
+      attr_accessor :max_width
 
       def initialize(config={})
-        @config = config
-        @config[:max_width] ||= 30
-        @config
+        @max_width = config[:max_width] || 30
       end
 
       def report
-        @max_time = all_resources.max_by{ |r| r.elapsed_time}.elapsed_time
-        @max_resource = all_resources.max_by{ |r| full_name(r).length}
         Chef::Log.info "%-#{max_resource_length}s  %s"%["Resource", "Elapsed Time"]
         Chef::Log.info "%-#{max_resource_length}s  %s"%["========", "============"]
         all_resources.each do |r|
           char = if r.updated then "+" else "-" end
-          bar = char * ( @config[:max_width] * (r.elapsed_time/@max_time))
+          bar = char * ( max_width * (r.elapsed_time/max_time))
           Chef::Log.info "%-#{max_resource_length}s  %s"%[full_name(r), bar]
         end
         Chef::Log.info ""
-        Chef::Log.info "Slowest Resource : #{full_name(@max_resource)} (%.6fs)"%[@max_time]
+        Chef::Log.info "Slowest Resource : #{full_name(max_resource)} (%.6fs)"%[@max_time]
         Chef::Log.info "Scale            : %.6fs per unit width"%[unit_width]
         Chef::Log.info " * '+' denotes a resource which updated this run"
         Chef::Log.info " * '-' denotes a resource which did not update this run"
       end
 
-    end
+      private
 
-    def unit_width
-      @max_time/@config[:max_width]
-    end
+      def unit_width
+        max_time/max_width
+      end
 
-    def max_resource_length
-      full_name(@max_resource).length
-    end
+      def max_resource_length
+        @max_resource_length ||= full_name(max_resource).length
+      end
 
-    def full_name(resource)
-      "#{resource.resource_name}[#{resource.name}]"
+      def full_name(resource)
+        "#{resource.resource_name}[#{resource.name}]"
+      end
+
+      def max_time
+        @max_time ||= all_resources.max_by{ |r| r.elapsed_time}.elapsed_time
+      end
+
+      def max_resource
+        @max_resource ||= all_resources.max_by{ |r| full_name(r).length}
+      end
     end
   end
 end
