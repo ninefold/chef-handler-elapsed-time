206c87d362a3764aafcef1c03a92c7ff4160bc26
Warren Bain <warren@ninefold.com>
2014-03-21 14:50:30 +1100

    Fix formatting of graph with ordering and proper method calls

--- lib/chef/handler/elapsed_time.rb
+++ lib/chef/handler/elapsed_time.rb
@@ -22,19 +22,19 @@ module Ninefold
 
       def initialize(config={})
         @max_width = config[:max_width] || 30
+        @max_length  = config[:max_length] || 120 - @max_width
       end
 
       def report
-        Chef::Log.info "%-#{max_resource_length}s  %s"%["Resource", "Elapsed Time"]
-        Chef::Log.info "%-#{max_resource_length}s  %s"%["========", "============"]
-        all_resources.each do |r|
+        Chef::Log.info "%-#{max_width}s %6s %-#{max_resource_length}-s"%["Elapsed Time", '', "Resource"]
+        Chef::Log.info "%-#{max_width}s %6s %-#{max_resource_length}-s"%["============", '', "========"]
+        all_resources.sort_by{ |r| r.elapsed_time }.each do |r|
           char = if r.updated then "+" else "-" end
-          bar = char * ( max_width * (r.elapsed_time/max_time))
-          Chef::Log.info "%-#{max_resource_length}s  %s"%[full_name(r), bar]
+          bar = char * ( max_width * (r.elapsed_time/max_time)).ceil
+          Chef::Log.info "%05.2fs %-#{max_width}s %-#{max_resource_length}-s"%[r.elasped_time, bar, full_name(r)]
         end
         Chef::Log.info ""
-        Chef::Log.info "Slowest Resource : #{full_name(max_resource)} (%.6fs)"%[@max_time]
-        Chef::Log.info "Scale            : %.6fs per unit width"%[unit_width]
+        Chef::Log.info "Scale : %.3fs per unit width"%[unit_width]
         Chef::Log.info " * '+' denotes a resource which updated this run"
         Chef::Log.info " * '-' denotes a resource which did not update this run"
       end
@@ -50,19 +50,19 @@ module Ninefold
       end
 
       def max_resource_length
-        @max_resource_length ||= full_name(max_resource).length
+        @max_resource_length ||= [ full_name(max_resource).length, max_length ].min
       end
 
       def full_name(resource)
         "#{resource.resource_name}[#{resource.name}]"
       end
 
-      def max_time
-        @max_time ||= all_resources.max_by{ |r| r.elapsed_time}.elapsed_time
+      def max_resource_time
+        @max_resource_time ||= max_resource.elapsed_time
       end
 
       def max_resource
-        @max_resource ||= all_resources.max_by{ |r| full_name(r).length}
+        @max_resource ||= all_resources.max_by{ |r| r.elapsed_time}
       end
 
       def full_name(resource)
