450ae56328a8b93cb090a2163fe162048d235016
Warren Bain <warren@ninefold.com>
2014-03-21 15:00:17 +1100

    Fix error in format string

--- lib/chef/handler/elapsed_time.rb
+++ lib/chef/handler/elapsed_time.rb
@@ -26,12 +26,12 @@ module Ninefold
       end
 
       def report
-        Chef::Log.info "%-#{max_width}s %6s %-#{max_resource_length}-s"%["Elapsed Time", '', "Resource"]
-        Chef::Log.info "%-#{max_width}s %6s %-#{max_resource_length}-s"%["============", '', "========"]
+        Chef::Log.info "%-#{max_width}s %6s %-#{max_resource_length}s"%["Elapsed Time", '', "Resource"]
+        Chef::Log.info "%-#{max_width}s %6s %-#{max_resource_length}s"%["============", '', "========"]
         all_resources.sort_by{ |r| r.elapsed_time }.each do |r|
           char = if r.updated then "+" else "-" end
           bar = char * ( max_width * (r.elapsed_time/max_time)).ceil
-          Chef::Log.info "%05.2fs %-#{max_width}s %-#{max_resource_length}-s"%[r.elasped_time, bar, full_name(r)]
+          Chef::Log.info "%05.2fs %-#{max_width}s %-#{max_resource_length}s"%[r.elasped_time, bar, full_name(r)]
         end
         Chef::Log.info ""
         Chef::Log.info "Scale : %.3fs per unit width"%[unit_width]
